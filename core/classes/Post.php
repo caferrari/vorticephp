8aeb46def837860850eeb39314438ec87dfcd531
caferrari <caferrari@e741a136-ebac-11dd-b2d9-215335d0b316>
2009-04-15 18:52:34 +0000

    campos com nome id ou id_* devem ser numericos, por restricao do framework
    git-svn-id: http://vortice.googlecode.com/svn/trunk@27 e741a136-ebac-11dd-b2d9-215335d0b316

--- core/classes/Post.php
+++ core/classes/Post.php
@@ -64,7 +64,7 @@ class Post
 		global $_PAR;
 		if (!is_array($_PAR)) $_PAR = array();
 		foreach (array_merge($_POST, $_GET, $_PAR) as $k => $v)
-			if ($v!='' && ereg("^id", $k) && !is_numeric($v)) throw new IntegerRequiredException("it can be a SQL injection try!");
+			if ($v!='' && ($k=="id" || ereg("^id_", $k)) && !is_numeric($v)) throw new IntegerRequiredException("it can be a SQL injection try!");
 		
 		self::$form = array();
 		
