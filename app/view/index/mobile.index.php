813aa5c38d37de3db04e05e5566940f129635335
Carlos André Ferrari <caferrari@gmail.com>
2009-10-21 15:59:55 -0300

    PHP 5.3 - Some php short tags removed.

--- app/view/index/mobile.index.php
+++ app/view/index/mobile.index.php
@@ -12,7 +12,7 @@
 ?>
 <h2>{{Página Inicial}} (Mobile)</h2>
 <ul id="menu-principal">
-	<li><a class="lnk1" href="<?= new Link("exemplo") ?>" title="{{Exemplo}}">{{Exemplo}}</a></li>
+	<li><a class="lnk1" href="<?php echo new Link("exemplo") ?>" title="{{Exemplo}}">{{Exemplo}}</a></li>
 </ul>
 <?php 
 include "info.php";
