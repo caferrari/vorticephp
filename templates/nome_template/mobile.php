ece068e451e414501e4454ee8e1e76bde666af33
Carlos André Ferrari <caferrari@gmail.com>
2009-10-21 15:55:43 -0300

    PHP 5.3 - Some php short tags removed.

--- templates/nome_template/mobile.php
+++ templates/nome_template/mobile.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /* 
  * Copyright (c) 2008, Carlos André Ferrari <[carlos@]ferrari.eti.br>; Luan Almeida <[luan@]luan.eti.br>
  * All rights reserved. 
@@ -10,7 +10,7 @@
  * @subpackage Template
  */
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?= request_lang?>" lang="<?= request_lang?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo request_lang?>" lang="<?php echo request_lang?>">
 <head>
 	<title>{{Exemplo de Sistema}} (Mobile)</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -23,9 +23,9 @@
 <body>
 <div id="conteiner">
 	<ul id="lang">
-		<li><a href="<?= rootvirtual . uri ?>" lang="<?=default_lang?>"><?=default_lang?></a></li>
-		<li><a href="<?= rootvirtual ?>en/<?= uri ?>" lang="en">en</a></li>
-		<li><a href="<?= rootvirtual ?>jp/<?= uri ?>" lang="jp">日本語</a></li>
+		<li><a href="<?php echo rootvirtual ?>" lang="pt-br">pt-br</a></li>
+		<li><a href="<?php echo rootvirtual ?>en/<?php uri ?>" lang="en">en</a></li>
+		<li><a href="<?php echo rootvirtual ?>jp/<?php uri ?>" lang="jp">日本語</a></li>
 	</ul>
 	<div id="topo">
 		<h1><a class="hidetxt" href="<? echo rootvirtual . request_lang ?>" title="{{Página inicial}}">{{Exemplo de Sistema}} (Mobile)</a></h1>
--- templates/nome_template/template.php
+++ templates/nome_template/template.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /* 
  * Copyright (c) 2008, Carlos André Ferrari <[carlos@]ferrari.eti.br>; Luan Almeida <[luan@]luan.eti.br>
  * All rights reserved. 
@@ -10,7 +10,7 @@
  * @subpackage Template
  */
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?= request_lang?>" lang="<?= request_lang?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo request_lang?>" lang="<?php echo request_lang?>">
 <head>
 	<title>{{Exemplo de Sistema}}</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -20,7 +20,7 @@
 	<meta name="language" content="<?= request_lang?>" />
 	<!--csstags-->
 	<script>
-		var rootvirtual = '<?= rootvirtual ?>';
+		var rootvirtual = '<?php echo rootvirtual ?>';
 	</script>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
@@ -29,9 +29,9 @@
 <body>
 <div id="conteiner">
 	<ul id="lang">
-		<li><a href="<?= rootvirtual . uri ?>" lang="<?=default_lang?>"><?=default_lang?></a></li>
-		<li><a href="<?= rootvirtual ?>en/<?= uri ?>" lang="en">en</a></li>
-		<li><a href="<?= rootvirtual ?>jp/<?= uri ?>" lang="jp">日本語</a></li>
+		<li><a href="<?php echo rootvirtual ?>" lang="pt-br">pt-br</a></li>
+		<li><a href="<?php echo rootvirtual ?>en/<?php uri ?>" lang="en">en</a></li>
+		<li><a href="<?php echo rootvirtual ?>jp/<?php uri ?>" lang="jp">日本語</a></li>
 	</ul>
 	<div id="topo">
 		<h1><a class="hidetxt" href="<? echo rootvirtual . request_lang ?>" title="{{Página inicial}}">{{Exemplo de Sistema}}</a></h1>
