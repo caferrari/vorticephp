601d2d474e8795d00762d77c7fe740f3e96b4f72
Carlos André Ferrari <andre@note.(none)>
2010-01-28 17:41:08 -0200

    Fixed the framework url in the sample app

--- app/webroot/templates/my_template/template.php
+++ app/webroot/templates/my_template/template.php
@@ -41,7 +41,7 @@
 		<!--mensagem-->
 		<!--content-->
 	</div>
-	<div id="rodape"><a href="http://github.com/caferrari/vorticephp2" title="Project at GitHub!">Check the project at Github</a></div>
+	<div id="rodape"><a href="http://github.com/caferrari/vorticephp" title="Project at GitHub!">Check the project at Github</a></div>
 </div>
 </body>
 </html>
