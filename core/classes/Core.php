1875c5491132f90d9f9f903e5d4c64ca4da66faa
Carlos André Ferrari <caferrari@gmail.com>
2010-02-04 10:49:21 -0300

    Added Env class to manage environment variables instead of constants

--- core/classes/Core.php
+++ core/classes/Core.php
@@ -39,22 +39,8 @@ class Core{
 		header('Content-type: text/html; charset=UTF-8');
 		header('X-Powered-By: VorticePHP');
 		
-		$tmproot = str_replace('\\', '/', dirname($_SERVER['SCRIPT_FILENAME']) . '/');
-		$tmproot = preg_replace('@/core/$@', '/', $tmproot);
-		
-		if (file_exists($tmproot . 'app/config.php')) include tmproot . 'app/config.php';
-		
-		if (!defined('root'))  define ('root', $tmproot);
-		if (!defined('virtualroot')) define ('virtualroot', preg_replace('@/+@', '/', preg_replace('@' . $_SERVER['DOCUMENT_ROOT'] . '|core/core.php@', '/', $_SERVER['SCRIPT_FILENAME'])));
-		if (!defined('webroot')) define ('webroot', root . 'app/webroot');
-		
-		if (!file_exists(root . 'environment')) $env = 'production';
-		else $env = file_get_contents(root . 'environment');
-
-		if (preg_match('@^([a-z0-9]+)@', $env, $mat))
-			define ('environment', $mat[0]);
-			
-		define ('production', environment=='production');
+		require_once 'Env.php';
+		Env::setup();
 		
 		require_once 'Crypt.php';
 		require_once 'Session.php';
@@ -64,20 +50,7 @@ class Core{
 		require_once 'Route.php';
 		require_once 'Post.php';
 
-		if (!defined('default_controller'))	define ('default_controller', 'index');
-		if (!defined('default_action')) 	define ('default_action', 'index');
-		if (!defined('default_lang')) 		define ('default_lang', 'pt-br');
-		if (!defined('apphash')) 		define ('apphash', md5(__FILE__));
-		
-		define ('windows', preg_match('@^[a-zA-Z]:@', __FILE__));
-		define ('ajax', isset($_SERVER['HTTP_X_REQUESTED_WITH']));
-		define ('post', $_SERVER['REQUEST_METHOD'] == 'POST');
-		define ('mobile', is_mobile());
-		define ('bot', is_bot());
-		
 		if (file_exists(root . 'app/functions.php')) include root . 'app/functions.php';
-
-		if (!defined('default_lang')) define('default_lang', 'pt-br');
 
 		Link::translateUri();
 
