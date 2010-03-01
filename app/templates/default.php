ea7d069143cb843ce97087ffa8503c02edd6cb0b
Carlos A. Ferrari <caferrari@gmail.com>
2010-03-01 11:43:45 -0300

    Merge and sync with changes made on website branch

--- app/templates/default.php
+++ app/templates/default.php
@@ -2,56 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Vórtice PHP Framework</title>
+<title>Vórtice PHP Framework Sample</title>
 	<!--csstags-->
 	<!--jstags-->
 </head>
 <body>
-	<div id="geral" class="container_12">
-		<div id="topo" class="grid_12">
-			<div id="logo">
-				<h1>Vórtice</h1>
-			</div>
-		
-			<div id="download">
-				<span class="txt">
-					<a href="http://github.com/caferrari/vorticephp/archives/master">Download</a>
-					<!--span><strong>2.700</strong> já baixaram</span-->
-				</span>
-			</div>
-		</div>
-	
-		<div id="menu" class="grid_3 m10">
-			<ul>
-				<li><a href="<?php echo new Link() ?>">inicial</a></li>
-				<li><a href="http://github.com/caferrari/vorticephp/archives/master">download</a></li>
-				<li><a href="<?php echo new Link('docs') ?>">documentação</a></li>
-				<li><a href="http://github.com/caferrari/vorticephp">source code</a></li>
-				<li><a href="http://www.youtube.com/view_play_list?p=71622ABD8E61AE6B&search_query=vorticephp&rclk=pti">screencasts</a></li>
-				<li><a href="<?php echo new Link('docs') ?>">blogs</a></li>
-				<!--li><a href="#">contato</a></li-->
-			</ul>
-		</div>
-	
-		<div id="conteudo" class="grid_9 m10">
-			<!--content-->
-	
-		</div>
-	
-		<div id="barra-final" class="grid_12 m20">
-			<p>© 2010 Vórtice - PHP Framework</p>
-			<p>Design por <a href="http://thiagofaleiro.com" target="_blank">Thiago Faleiro</a></p>
-		</div>
+	<h1>Template Sample</h1>
+	<!--content-->
 
-	</div>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-695381-21");
-pageTracker._trackPageview();
-} catch(err) {}</script>
 </body>
 </html>
