9abb62b6b09c89fd0abd979ae97dd08d51bac025
Carlos André Ferrari <caferrari@gmail.com>
2010-03-04 07:01:06 -0300

    Updated the template

--- app/templates/default.php
+++ app/templates/default.php
@@ -2,13 +2,162 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Vórtice PHP Framework Sample</title>
-	<!--csstags-->
-	<!--jstags-->
+<title>Vórtice PHP Framework</title>
+	<link href="<?php echo virtualroot ?>templates/default/css/grid.css?1264907226" rel="stylesheet" media="screen" />
+	<link href="<?php echo virtualroot ?>templates/default/css/layout.css?1264907226" rel="stylesheet" media="screen" />
+	<script type="text/javascript" src="<?php echo virtualroot ?>templates/default/js/jquery-1.3.2.min.js?1264907226"></script>
+	<script type="text/javascript" src="<?php echo virtualroot ?>templates/default/js/scripts.js?1264907226"></script>
 </head>
-<body>
-	<h1>Template Sample</h1>
-	<!--content-->
 
+<body>
+	<div id="geral" class="container_12">
+		<div id="topo" class="grid_12">
+			<div id="logo">
+				<h1>Vórtice</h1>
+			</div>
+		
+			<div id="download">
+				<span class="txt">
+					<a href="http://github.com/caferrari/vorticephp/archives/master">Download</a>
+
+					<!--span><strong>2.700</strong> já baixaram</span-->
+				</span>
+			</div>
+		</div>
+	
+		<div id="menu" class="grid_3 m10">
+			<ul>
+				<li><a href="/">inicial</a></li>
+				<li><a href="http://github.com/caferrari/vorticephp/archives/master">download</a></li>
+
+				<li><a href="/docs/">documentação</a></li>
+				<li><a href="http://github.com/caferrari/vorticephp">source code</a></li>
+				<li><a href="http://www.youtube.com/view_play_list?p=71622ABD8E61AE6B&search_query=vorticephp&rclk=pti">screencasts</a></li>
+				<li><a href="/docs/">blogs</a></li>
+				<!--li><a href="#">contato</a></li-->
+			</ul>
+		</div>
+
+	
+		<div id="conteudo" class="grid_9 m10">
+				
+			<p class="intro">Um <strong>framework PHP</strong> feito pensando-se em performance e liberdade para o programador, tem tudo para o desenvolvimento de aplicações seguindo os padrões mais requisitados do mercado, porém, se o desenvolvedor quiser criar um código <em>spaghetti</em>, ele pode!.</p>
+	
+			<div id="box-recursos" class="m20">
+				<span class="box-titulo">Recursos</span>
+				<ul class="container">
+
+					<li class="grid_3 omega">
+						<h2>Velocidade</h2>
+						<p>O Vórtice é extremamente veloz, e tudo é opcional, se algo não existe em sua pasta de app, ele simplesmente ignora!</p>
+					</li>
+					<li class="grid_3 alpha">
+						<h2>URL's Amigáveis</h2>
+						<p>Padrão de url que, além de aumentar a seguranca por filtrar os dados, a url fica muito mais legível e o google adora!</p>
+
+					</li>
+					<li class="grid_3">
+						<h2>Internacionalização</h2>
+						<p>Seu site em várias linguas sem dificuldades, e o google indexa todas as traduçoes individualmente</p>
+					</li>
+					<li class="grid_3 omega">
+						<h2>Suporte a Ajax!</h2>
+
+						<p>O Vórtice foi feito pensando-se em Ajax, ele consegue identificar internamente se a requisição é por ajax e já devolve no formato correto</p>
+					</li>
+			
+					<li class="grid_3 alpha">
+						<h2>Utilitários para Post</h2>
+						<p>O Vórtice possúi mecanismos que auxiliam muito na parte de criação, validacao e retornos de formulários</p>
+					</li>
+					<li class="grid_3">
+
+						<h2>Sessões Encriptadas</h2>
+						<p>As sessões dos visitantes são encriptados através de chaves aleatórias por visita</p>
+					</li>
+					<li class="grid_3 omega">
+						<h2>Views Estáticas</h2>
+						<p>Se uma página específica não acessa banco de dados ou não precisa de nenhum processamento no servidor, isso é a solução</p>
+					</li>
+
+			
+					<li class="grid_3 alpha">
+						<h2>Snippets</h2>
+						<p>Para trechos de código muito comuns, você pode criar snippets!</p>
+					</li>
+					<li class="grid_3">
+						<h2>Camada Facade (MVCF)</h2>
+						<p>Com essa camada você pode encapsular mais o seu código, agrupando varias açoes em um facade.</p>
+
+					</li>
+				</ul>
+		
+				<!--p class="btn1">Botão 1</p>
+				<p class="btn2">Botão 2</p-->
+		
+				<div id="r-menu">
+					<span>1</span> <span>2</span> <span>3</span>
+				</div>		
+			</div>
+
+	
+			<div id="twitter" class="grid_6 alpha m20">
+				<span class="box-titulo">Twitter</span>
+				<ul>
+					<li>
+						<span class="data">29 Dez / 2010</span>
+						<h3><a href="#">In a orci purus. Morbi massa dolor, luctus at auctor non, varius pulvinar nunc. Etiam at mi id nisi egestas convallis eu.</a></h3>
+					</li>
+
+					<li>
+						<span class="data">29 Dez / 2010</span>
+						<h3><a href="#">In a orci purus. Morbi massa dolor, luctus at auctor non, varius pulvinar nunc. Etiam at mi id nisi egestas convallis eu.</a></h3>
+					</li>
+					<li>
+						<span class="data">29 Dez / 2010</span>
+						<h3><a href="#">In a orci purus. Morbi massa dolor, luctus at auctor non, varius pulvinar nunc. Etiam at mi id nisi egestas convallis eu.</a></h3>
+
+					</li>
+					<li>
+						<span class="data">29 Dez / 2010</span>
+						<h3><a href="#">In a orci purus. Morbi massa dolor, luctus at auctor non, varius pulvinar nunc. Etiam at mi id nisi egestas convallis eu.</a></h3>
+					</li>
+				</ul>
+			</div>
+	
+			<div id="comentarios" class="grid_3 omega m20">
+
+				<div class="container">
+					<span class="box-titulo">Comentários</span>
+					<ul>
+						<li>
+							<p>O Vórtice ainda é pequeno e precisa de ajuda, principalmente na parte de documentação. me adicione no gtalk e vamos conversar.</p>
+							<p class="autor"><a href="http://ferrari.eti.br">Carlos André Ferrari</a><span>desenvolvedor da SECOM/TO</span></p>
+						</li>
+
+					</ul>
+				</div>
+			</div>
+
+	
+		</div>
+	
+		<div id="barra-final" class="grid_12 m20">
+			<p>© 2010 Vórtice - PHP Framework</p>
+			<p>Design por <a href="http://thiagofaleiro.com" target="_blank">Thiago Faleiro</a></p>
+
+		</div>
+
+	</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-695381-21");
+pageTracker._trackPageview();
+} catch(err) {}</script>
 </body>
 </html>
+
