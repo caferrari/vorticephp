69fa2b456ba11bf11421db8e24bc99034a2c091c
Carlos André Ferrari <caferrari@gmail.com>
2011-04-22 08:49:02 -0300

    corrigido bug na classe Route

--- core/classes/Route.php
+++ core/classes/Route.php
@@ -43,7 +43,7 @@ class Route{
 	public static function exec(){
 		list ($uri) = explode('?', uri);
 		foreach (self::$routes as $r){
-			$tmp_uri = (strstr($r['er'], '\?') === false) ? $uri : uri;
+			//$tmp_uri = (strstr($r['er'], '\?') === false) ? $uri : uri;
 			if (preg_match($r['er'], $tmp_uri, $match)){
 				$p = $r['pars'];
 				for ($x=1; $x<count($match); $x++) $p = str_replace('%' . $x, $match[$x], $p);
