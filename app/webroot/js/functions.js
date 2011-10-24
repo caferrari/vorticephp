96601b01067272ce841a92a0d1b955e8482c1818
Carlos André Ferrari <andre@rizza.(none)>
2011-10-24 09:39:57 -0300

    Cleaned up

--- app/webroot/js/functions.js
+++
@@ -1,9 +0,0 @@
-function focus() {
-  if(document.forms && document.forms.length > 0 && document.forms[0].elements)  {
-     for(i = 0; i < document.forms[0].elements.length; i++)
-       if(document.forms[0].elements[i].type != "hidden") {
-         document.forms[0].elements[i].focus();
-         return;
-       }
-  }
-}
